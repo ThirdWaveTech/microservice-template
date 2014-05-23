a4f014a5b09f8cd61d63fb8364daa3366b8e55fb
Rob Scaduto <rscaduto@core4tek.com>
2014-05-23 08:55:17 -0400

    Added connection string configs

---
+++ src/__NAME__.Database/Deploy.ps1
@@ -0,0 +1,7 @@
+﻿.\Migrate.exe --configPath __NAME__.Database.dll.config `
+	--connection "__NAME__" `
+	--provider sqlserver2012 `
+	--target "__NAME__.Database.dll" `
+	--output --outputFilename migrate-output.sql --verbose=true `
+	--task migrate `
+	--timeout=600
