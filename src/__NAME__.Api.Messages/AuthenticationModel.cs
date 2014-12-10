9bd886700b9877b20aaa31b93ac6ee99f93818bb
lhernandez <leo@booleansolutions.com>
2014-12-09 20:38:29 -0500

    Added messages project

---
+++ src/__NAME__.Api.Messages/AuthenticationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace __NAME__.Api.Messages
+{
+    public class AuthenticationModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
