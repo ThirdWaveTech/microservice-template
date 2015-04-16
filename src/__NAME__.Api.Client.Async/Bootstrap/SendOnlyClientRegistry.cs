4a29b165514781ced47cfd309d419f2edfa620b8
Denis Zabavchik <DZabavchik@implexsoft.com>
2015-04-16 10:18:34 -0400

    Sub for async client registry

---
+++ src/__NAME__.Api.Client.Async/Bootstrap/SendOnlyClientRegistry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Features;
+using StructureMap.Configuration.DSL;
+
+namespace __NAME__.Api.Client.Async.Bootstrap
+{
+    class SendOnlyClientRegistry: Registry
+    {
+        public SendOnlyClientRegistry()
+        {
+            //NServiceBus.Logging.LogManager.Use<Log4NetFactory>();
+
+            var configuration = new BusConfiguration();
+
+            //configuration.UseContainer<StructureMapBuilder>(c => c.ExistingContainer(ObjectFactory.Container));
+            //configuration.AssembliesToScan(new Assembly[] { typeof(ReportReceivedMessage).Assembly });
+            configuration.UseSerialization<JsonSerializer>();
+            configuration.UsePersistence<InMemoryPersistence>();
+            configuration.DisableFeature<Sagas>();
+            configuration.DisableFeature<MessageDrivenSubscriptions>();
+            configuration.DisableFeature<TimeoutManager>();
+            var bus = Bus.CreateSendOnly(configuration);
+
+            ForSingletonOf<ISendOnlyBus>().Use(bus);
+
+        }
+    }
+}
