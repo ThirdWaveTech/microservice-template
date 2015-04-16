276886982485b76a31ac56992beb0bc22f36f2c4
Rob Scaduto <rob@thirdwave.it>
2015-04-16 16:59:36 -0400

    Added client interface, automappings

---
+++ src/__NAME__.Api/Mappings/ExampleMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Crux.Core.Bootstrapping;
+using __NAME__.Domain;
+using __NAME__.Models.Example;
+
+namespace __NAME__.Api.Mappings
+{
+    public class ExampleMapper : IRunAtStartup
+    {
+        public void Init()
+        {
+            Mapper.CreateMap<ExampleEntity, ExampleModel>();
+        }
+    }
+}
