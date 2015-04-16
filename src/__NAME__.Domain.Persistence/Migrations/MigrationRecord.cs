3a4155a120bf91f23a2feef88ee7fa00e8d8d72a
Denis Zabavchik <DZabavchik@implexsoft.com>
2015-04-16 16:19:49 -0400

    Renamed message bus client template class

---
+++ src/__NAME__.Domain.Persistence/Migrations/MigrationRecord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __NAME__.Domain.Persistence.Migrations
+{
+    class MigrationRecord
+    {
+    }
+}
